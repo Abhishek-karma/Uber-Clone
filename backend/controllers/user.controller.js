890087dc0524d27b9af85a19b75dc5743ce3a545
Abhishek-karma <abhikarma.work@gmail.com>
2024-12-08 16:40:16 +0530

    user login done

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -28,3 +28,29 @@ module.exports.registerUser = async (req, res, next) => {
     res.status(500).json({ error: "Server Error" });
   }
 };
+
+
+module.exports.loginUser = async (req, res, next) => { 
+    const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() })
+  }
+    const { email, password } = req.body;
+
+    const user = await userModel.findOne({ email }).select("+password");
+
+    if(!user){
+        return res.status(401).json({ error: "Invalid Credentials"})
+    }
+
+    const isMatch = await user.comparePassword(password);
+
+    if (!isMatch) {
+      return res.status(400).json({ error: "Invalid Credentials" })
+    }
+
+    const token = user.generateAuthToken();
+    res.status(200).json({token,user});
+};
+
