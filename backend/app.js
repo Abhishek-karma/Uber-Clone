9fe43c3009350f8c58ef9883463b18df88b1e509
Abhishek-karma <abhikarma.work@gmail.com>
2024-12-09 20:43:41 +0530

    user authentication done

--- backend/app.js
+++ backend/app.js
@@ -4,13 +4,14 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 const connectToDb = require('./db/db');
 const userRoutes = require('./routes/user.routes');
+const cookie = require('cookie-parser')
 connectToDb();
 
 dotenv.config();
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true}))
-
+app.use(cookie());
 
 app.get('/',(req,res)=>{
     res.send("Hello World");
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { body } = require("express-validator");
 const userController = require("../controllers/user.controller");
-
+const authMiddleware = require('../middlerwares/auth.middleware')
 router.post(
   "/register",
   [
@@ -25,5 +25,9 @@ router.post(
   ],
   userController.loginUser
 );
+
+router.get('/profile',authMiddleware.authUser,userController.getUserProfile);
+
+router.get('/logout',authMiddleware.authUser ,userController.logOut)
 
 module.exports = router;
